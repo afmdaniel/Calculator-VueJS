c3a61270b918c226d2cd9d8e35ad0ca3a0a882b7
afmdanielwin <danielfarias380@gmail.com>
2020-05-08 18:44:34 -0300

    Emit event to parent component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,53 +1,51 @@
 <template>
-  <button class="button" :class="{ double, triple, operation }">
-      {{ label }}
-  </button>
+  <button
+    class="button"
+    :class="{ double, triple, operation }"
+    @click="(e) => $emit('click', label)"
+  >{{ label }}</button>
 </template>
 
 <script>
 export default {
-    props: {
-        label: null,
-        operation: Boolean,
-        double: Boolean,
-        triple: Boolean
-    }
-}
+  props: {
+    label: null,
+    operation: Boolean,
+    double: Boolean,
+    triple: Boolean
+  }
+};
 </script>
 
 <style>
 :root {
-    --bg-button: #f0f0f0;
-    --border-button: solid 1px #888;
+  --bg-button: #f0f0f0;
+  --border-button: solid 1px #888;
 }
 
 .button {
-    font-size: 1.4em;
-    background-color: var(--bg-button);
-    border: none;
-    border-right: var(--border-button);
-    border-bottom: var(--border-button);
-    outline: none;
+  font-size: 1.4em;
+  background-color: var(--bg-button);
+  border: none;
+  border-right: var(--border-button);
+  border-bottom: var(--border-button);
+  outline: none;
 }
 
 .button:active {
-    background-color: #ccc;
+  background-color: #ccc;
 }
 
 .button.double {
-    grid-column: span 2;
-}
-
-.button.triple {
-    grid-column: span 3;
+  grid-column: span 2;
 }
 
 .button.operation {
-    background-color: #fa8231;
-    color: #fff
+  background-color: #fa8231;
+  color: #fff;
 }
 
 .button.operation:active {
-    background-color: #fa8231cc;
+  background-color: #fa8231cc;
 }
 </style>
