20a3a36877956e471964188e8446bbc472f92ad0
afmdanielwin <danielfarias380@gmail.com>
2020-05-08 22:33:58 -0300

    Logic implementation

--- src/main/Calculator.vue
+++ src/main/Calculator.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="calculator">
-    <Display value="1000" />
+    <Display :value="displayValue" />
     <Button label="AC" @click="clearMemory" />
     <Button label="±" @click="addDigit" />
     <Button label="%" @click="setOperation" />
-    <Button label="/" operation @click="setOperation" />
+    <Button label="÷" operation @click="setOperation" />
     <Button label="7" @click="addDigit" />
     <Button label="8" @click="addDigit" />
     <Button label="9" @click="addDigit" />
-    <Button label="*" operation @click="setOperation" />
+    <Button label="x" operation @click="setOperation" />
     <Button label="4" @click="addDigit" />
     <Button label="5" @click="addDigit" />
     <Button label="6" @click="addDigit" />
@@ -32,15 +32,90 @@ const Calculator = {
     Button,
     Display
   },
+  data: function() {
+    return {
+      displayValue: "0",
+      clearDisplay: false,
+      operation: null,
+      values: [0, 0],
+      currentIndex: 0
+    };
+  },
   methods: {
     clearMemory: function() {
-      console.log("Limpar memória!");
+      Object.assign(this.$data, this.$options.data());
     },
     setOperation: function(operation) {
-      console.log("Operação " + operation);
+      let operationResolved = false;
+      const values = [...this.values];
+      
+      if (this.currentIndex !== 0) {
+        const currentOperation = this.operation;
+
+        switch (currentOperation) {
+          case "%":
+            values[0] *= values[1] / 100;
+            break;
+          case "÷":
+            values[0] /= values[1];
+            values[0] = parseFloat(values[0].toFixed(3));
+            break;
+          case "x":
+            values[0] *= values[1];
+            break;
+          case "+":
+            values[0] += values[1];
+            break;
+          case "-":
+            values[0] -= values[1];
+            break;
+          default:
+        }
+
+        operationResolved = operation === "=";
+        values[1] = 0;
+      }
+
+      this.displayValue = values[0].toString();
+      this.clearDisplay = true;
+      this.operation = operationResolved ? null : operation;
+      this.values = values;
+      this.currentIndex = operationResolved ? 0 : 1;
     },
     addDigit(n) {
-      console.log("Digito " + n);
+      if (n === "." && this.displayValue.includes(".")) {
+        return;
+      }
+
+      let currentValue;
+      let displayValue;
+
+      if (n === "±") {
+        currentValue = this.displayValue;
+        displayValue =
+          currentValue.indexOf("-") === -1
+            ? "-" + currentValue
+            : currentValue.replace("-", "");
+      } else {
+        const regex = /-?0/;
+        const clearDisplay = regex.test(this.displayValue) || this.clearDisplay;
+
+        if (this.displayValue.indexOf("-") !== -1 && !this.clearDisplay) {
+          currentValue = clearDisplay ? "-" : this.displayValue;
+        } else {
+          currentValue = clearDisplay ? "" : this.displayValue;
+        }
+        
+        displayValue = currentValue + n;
+      }
+
+      if (n !== ".") {
+        const newValue = parseFloat(displayValue);
+        this.values[this.currentIndex] = newValue;
+      }
+
+      this.displayValue = displayValue;
+      this.clearDisplay = false;
     }
   }
 };
